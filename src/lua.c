f05fc01324838bd6f6c2e6742fde75e9b777eb8a
Caleb Anderson <caleb@mint-virtual-machine.(none)>
2012-09-21 18:07:19 +1000

    initial lua

---
+++ src/lua.c
@@ -0,0 +1,94 @@
+#include "lua.h"
+#include "engine.h"
+lua_State *L;
+
+void setup_lua(){
+  L = lua_open();   /* opens Lua */
+  luaopen_base(L);             /* opens the basic library */
+  luaopen_table(L);            /* opens the table library */
+  luaopen_io(L);               /* opens the I/O library */
+  luaopen_string(L);           /* opens the string lib. */
+  luaopen_math(L);             /* opens the math lib. */
+  static const struct luaL_reg faster [] = {
+    {"targets", l_targets},
+    {NULL, NULL}  /* sentinel */
+  };
+
+
+  /* register target */
+
+  static const struct luaL_reg target_f [] = {
+    {"new", newarray},
+    {NULL, NULL}
+  };
+
+  static const struct luaL_reg target_m [] = {
+    {"host", l_target_host},
+    {"port", l_target_port},
+    {"path", l_target_path},
+    {"ssl", l_target_ssl},
+    {NULL, NULL}
+  };
+
+  luaL_newmetatable(L, "faster.target");
+  lua_pushstring(L, "__index");
+  lua_pushvalue(L, -2);  /* pushes the metatable */
+  lua_settable(L, -3);  /* metatable.__index = metatable */
+  luaL_openlib(L, NULL, target_m, 0);
+  luaL_openlib(L, NULL, target_f, 0);
+}
+
+static int l_targets (lua_State *L) {
+  struct target *t;
+  lua_newtable(L);
+  i=1;
+  for(t=get_targets();t;t=t->hh.next){
+    target2lua(L,t);
+    lua_pushinteger(L,i);
+    lua_settable(L,-3);
+    i++;
+  }
+  return 1;
+}
+
+static void target2lua(lua_State *L, struct target *t) {
+  struct target **ptrptr=(struct target **) lua_newuserdata(L, sizeof(struct target *));
+  *ptrptr=t;
+  luaL_getmetatable(L, "faster.target");
+  lua_setmetatable(L, -2);
+}
+
+static struct target *check_target(lua_State *L){
+   void *ud = luaL_checkudata(L, 1, "faster.target");
+   luaL_argcheck(L, ud != NULL, 1, "`faster.target' expected");
+   return *((struct target **) ud);
+}
+
+static int l_target_host(lua_State *L){
+  struct target *t=check_target(L);
+  lua_pushstring(L, t->host);
+  return 1;
+}
+
+
+
+static int l_target_port(lua_State *L){
+  struct target *t=check_target(L);
+  lua_pushinteger(L, t->prototype_request->port);
+  return 1;
+}
+
+
+static int l_target_path(lua_State *L){
+  struct target *t=check_target(L);
+  lua_pushstring(L, serialize_path(t->prototype_request,0,0));
+  return 1;
+}
+
+static int l_target_ssl(lua_State *L){
+  struct target *t=check_target(L);
+  lua_pushinteger(L, t->prototype_request->proto==2);
+  return 1;
+}
+
+
---
+++ src/lua.h
@@ -0,0 +1,14 @@
+
+#ifndef FASTERPUSSYCAT_LUA_H
+#define FASTERPUSSYCAT_LUA_H
+#include "db.h"
+#include "engine.h"
+#include "lua51/lua.h"
+
+extern lua_State *L;
+
+/* lua.c */
+void setup_lua(void);
+
+
+#endif
